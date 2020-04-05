55882b19660a1e02faaa9ba219a427df8630e272
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 15:35:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PastryShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int numberChefs = int.Parse(Console.ReadLine());
+            int Cakes = int.Parse(Console.ReadLine());
+            int Waffles = int.Parse(Console.ReadLine());
+            int Pancakes = int.Parse(Console.ReadLine());
+
+            decimal sumForDay = (Cakes * 45.00m + Waffles * 5.80m + Pancakes * 3.20m) * numberChefs;
+
+            decimal sum = sumForDay * days;
+
+            Console.WriteLine($"{sum} FULLSUM={ Math.Round(sum-sum/8, 2)} BGN");
+
+        }
+    }
+}
