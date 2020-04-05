05a806f9cad9ee78751341aa815a4ed830ee9e5d
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 15:49:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Брой дни:");
+            ushort days = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Брой сладкари:");
+            ushort sladkari = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Брой торти:");
+            ushort cakes = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Брой гофрети:");
+            ushort gofreti = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Брой палачинки:");
+            ushort pancakes = ushort.Parse(Console.ReadLine());
+
+            decimal sumOfCakes = cakes * 45;
+            decimal sumOfPancakes = pancakes * 3.20m;
+            decimal sumOfGofreti = gofreti * 5.80m;
+
+            decimal sumForTheDay = (sumOfCakes + sumOfPancakes + sumOfGofreti) * sladkari;
+            decimal sumOfAllMoney = sumForTheDay * days;
+
+            decimal result = sumOfAllMoney - (sumOfAllMoney / 8);
+
+            Console.WriteLine($"Събрани средства: {result}");
+
+
+        }
+    }
+}
